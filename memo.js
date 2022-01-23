49038ac42bc6a588811fb59db0c7f425794c44d3
Paulo Victor <41065334+pvictorf@users.noreply.github.com>
2022-01-23 18:27:49 -0300

    Update memo.js

--- memo.js
+++ memo.js
@@ -3,9 +3,15 @@ export const memo = (fn) => {
 
   return function(...args) {
     const key = JSON.stringify(args)
+    
+    // Verificando se a função foi executada anteriormente com os mesmo argumentos
     if(!cache.has(key)) {
+    
+      // Executando a função e guardando o resultado e sua chave no cache.
       cache.set(key, fn(...args))
     }
+    
+    // Retornando diretamente o resultado guardado em cache.
     return cache.get(key)
   }
 }
